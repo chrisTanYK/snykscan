399a188e9a6cf162a5004e140f2134977f232082
chrisTanYK <christan_yaokun82@hotmail.com>
2025-05-14 14:40:44 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -11,19 +15,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_lambda_function" "hello_world" {
-  function_name = "helloWorld"
-  handler       = var.lambda_handler
-  runtime       = var.lambda_runtime
-  role          = aws_iam_role.lambda_exec.arn
-
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-}
+# Create a unique suffix for IAM role name
 resource "random_id" "role_suffix" {
   byte_length = 4
 }
 
+# IAM role for Lambda execution
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role_${random_id.role_suffix.hex}"
 
@@ -41,8 +38,25 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
-data "archive_file" "app_py.zip" {
+# Create ZIP archive of Lambda code
+data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda/hello-world"
-  output_path = "${path.module}/lambda/hello-world.zip"
+  source_file = "${path.module}/app.py"
+  output_path = "${path.module}/app_py.zip"
+}
+
+# Lambda function
+resource "aws_lambda_function" "hello_world" {
+  function_name = "helloWorld"
+  handler       = var.lambda_handler
+  runtime       = var.lambda_runtime
+  role          = aws_iam_role.lambda_exec.arn
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  depends_on = [
+    data.archive_file.lambda_zip,
+    aws_iam_role.lambda_exec
+  ]
 }
